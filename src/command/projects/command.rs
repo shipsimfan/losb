29af36c7933e69da31fe0918c9b46a9e332e4c4b
Lance Hart <shipsimfan@hotmail.com>
2021-07-23 23:42:19 -0400

    Added environment variables

--- src/command/projects/command.rs
+++ src/command/projects/command.rs
@@ -1,6 +1,7 @@
 pub struct Command {
     command: String,
     args: Vec<String>,
+    env: Option<Vec<(String, String)>>,
 }
 
 impl Command {
@@ -23,19 +24,49 @@ impl Command {
             }
         }
 
-        Ok(Command::new(command, args))
+        let env = match object.remove("env") {
+            None => None,
+            Some(object) => {
+                let mut env = Vec::new();
+                let vec = object.to_vec()?;
+                let mut iter = vec.iter();
+                while let Some(object) = iter.next() {
+                    let key = object.clone().to_string()?;
+                    let value = match iter.next() {
+                        Some(value) => value.clone().to_string()?,
+                        None => return Err(Box::new(crate::error::Error::InvalidCommand(key))),
+                    };
+
+                    env.push((key, value));
+                }
+
+                Some(env)
+            }
+        };
+
+        Ok(Command::new(command, args, env))
     }
 
-    fn new(command: String, args: Vec<String>) -> Self {
+    fn new(command: String, args: Vec<String>, env: Option<Vec<(String, String)>>) -> Self {
         Command {
             command: command,
             args: args,
+            env: env,
         }
     }
 
     pub fn create_command(&self) -> std::process::Command {
         let mut command = std::process::Command::new(self.command.to_string());
         command.args(self.args.clone());
+
+        match &self.env {
+            Some(env) => {
+                for env in env {
+                    command.env(&env.0, &env.1);
+                }
+            }
+            None => {}
+        }
 
         command.stdout(std::process::Stdio::inherit());
         command.stderr(std::process::Stdio::inherit());
