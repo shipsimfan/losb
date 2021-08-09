8ffc9f2f4d8553b779bf022331ac6211815a7f2a
Lance Hart <shipsimfan@hotmail.com>
2021-08-09 15:59:14 -0400

    Implemented integration of build

--- src/build.rs
+++ src/build.rs
@@ -1,13 +1,83 @@
+use std::{
+    env::current_dir,
+    fs::{copy, create_dir_all},
+    path::Path,
+    process::{Command, Stdio},
+};
+
+fn build_cargo(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut command = Command::new("cargo");
+    command.arg("build");
+    command.current_dir(path);
+    command.stdout(Stdio::inherit());
+    command.stderr(Stdio::inherit());
+    command.stdin(Stdio::inherit());
+    command.output()?;
+    Ok(())
+}
+
+fn install_build(
+    path: &str,
+    prefix: &Path,
+    sysroot: &Path,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut command = Command::new("build");
+    command.args([
+        "install",
+        "--sysroot",
+        sysroot.to_str().unwrap(),
+        "--prefix",
+        prefix.to_str().unwrap(),
+    ]);
+    command.current_dir(path);
+    command.stdout(Stdio::inherit());
+    command.stderr(Stdio::inherit());
+    command.stdin(Stdio::inherit());
+    command.output()?;
+    Ok(())
+}
+
 pub fn build() -> Result<(), Box<dyn std::error::Error>> {
     // Prepare sysroot
+    create_dir_all("./sysroot/los/bin")?;
+    create_dir_all("./sysroot/los/lib")?;
+    create_dir_all("./sysroot/los/include")?;
+    create_dir_all("./sysroot/EFI/BOOT")?;
 
-    // Build bootloader and kernel
+    // Build bootloader
+    println!("Building bootloader . . .");
+    build_cargo("./bootloader")?;
 
-    // Install bootloader and kernel
+    // Install bootloader
+    println!("\nInstalling bootloader . . .");
+    copy(
+        "./bootloader/target/x86_64-unknown-uefi/debug/bootloader.efi",
+        "./sysroot/EFI/BOOT/BOOTX64.EFI",
+    )?;
 
-    // Build and Install libraries
+    // Build kernel
+    println!("\nBuilding kernel . . .");
+    build_cargo("./kernel")?;
 
-    // Build and Install programs
+    // Install kernel
+    println!("\nInstalling kernel . . .");
+    copy(
+        "./kernel/target/x86_64-los/debug/kernel",
+        "./sysroot/kernel.elf",
+    )?;
+
+    // Prepare prefix and sysroot
+    let current_path = current_dir()?;
+    let prefix = current_path.join("sysroot/los");
+    let sysroot = current_path.join("sysroot");
+
+    // Build and install libraries
+    println!("\nBuilding libraries . . .");
+    install_build("./libraries", &prefix, &sysroot)?;
+
+    // Build and install programs
+    println!("\nBuilding programs . . .");
+    install_build("./programs", &prefix, &sysroot)?;
 
     Ok(())
 }
--- src/clean.rs
+++ src/clean.rs
@@ -1,15 +1,57 @@
+use std::{
+    fs::remove_dir_all,
+    path::Path,
+    process::{Command, Stdio},
+};
+
+fn clean_cargo(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut command = Command::new("cargo");
+    command.arg("clean");
+    command.current_dir(path);
+    command.stdout(Stdio::inherit());
+    command.stderr(Stdio::inherit());
+    command.stdin(Stdio::inherit());
+    command.output()?;
+    Ok(())
+}
+
+fn clean_build(path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut command = Command::new("build");
+    command.arg("clean");
+    command.current_dir(path);
+    command.stdout(Stdio::inherit());
+    command.stderr(Stdio::inherit());
+    command.stdin(Stdio::inherit());
+    command.output()?;
+    Ok(())
+}
+
 pub fn clean_user() -> Result<(), Box<dyn std::error::Error>> {
     // Remove sysroot
+    println!("Cleaning sysroot . . .");
+    if Path::new("./sysroot").exists() {
+        remove_dir_all("./sysroot")?;
+    }
 
     // Clean libraries
+    println!("Cleaning libraries . . .");
+    clean_build("./libraries")?;
 
     // Clean programs
+    println!("Cleaning programs . . .");
+    clean_build("./programs")?;
 
     Ok(())
 }
 
 pub fn clean() -> Result<(), Box<dyn std::error::Error>> {
-    // Clean kernel and bootloader
+    // Clean bootloader
+    println!("Cleaning bootloader . . .");
+    clean_cargo("./bootloader")?;
+
+    // Clean kernel
+    println!("Cleaning kernel . . .");
+    clean_cargo("./kernel")?;
 
     // Clean user
     clean_user()
